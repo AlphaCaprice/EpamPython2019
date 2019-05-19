17600b303c232a911fa8109fe655e811d0d89131
alpha_caprice <romantcaregorodtcev@gmail.com>
2019-05-19 21:36:07 +0300

    1.Three functions that checked if two strings are permutation were realized.

--- 00-Introduction-to-Python/hw.py
+++ 00-Introduction-to-Python/hw.py
@@ -6,10 +6,56 @@
 изменение порядка символов. Регистр учитывается, пробелы 
 являются существенными.
 """
+from collections import defaultdict, Counter
 
+# С использованием встроенного словаря
 def is_permutation(a: str, b: str) -> bool:
-    # Нужно проверить, являются ли строчки 'a' и 'b' перестановками
+    letter_counter = {}
+    if len(a) == len(b):
+        for letter in a:
+            if letter in letter_counter:
+                letter_counter[letter] += 1
+            else:
+                letter_counter[letter] = 1
+        for letter in b:
+            if letter in letter_counter:
+                letter_counter[letter] -= 1
+            else:
+                return False
+        return not any(letter_counter.values())
+    else:
+        return False
+
+# С использование модуля collections.Counter
+def is_permutation_2(a: str, b: str) -> bool:
+    if len(a) == len(b):
+        letter_counter = Counter(a)
+        letter_counter.subtract(Counter(b))
+        return not any(letter_counter.values())
+    else:
+        return False
+
+# С использование модуля collections.defaultdict
+def is_permutation_3(a: str, b: str) -> bool:
+    letter_counter = defaultdict(int)
+    if len(a) == len(b):
+        for letter in a:
+            letter_counter[letter] += 1
+        for letter in b:
+            letter_counter[letter] -= 1
+        return not any(letter_counter.values())
+    else:
+        return False
 
 
-assert is_permutation('baba', 'abab')
-assert is_permutation('abbba', 'abab')
+print(is_permutation('baba', 'abab'))
+print(is_permutation('abbba', 'abab'))
+print(is_permutation('baba', 'abbb'))
+print("---")
+print(is_permutation_2('baba', 'abab'))
+print(is_permutation_2('abbba', 'abab'))
+print(is_permutation_2('baba', 'abbb'))
+print("---")
+print(is_permutation_3('baba', 'abab'))
+print(is_permutation_3('abbba', 'abab'))
+print(is_permutation_3('baba', 'abbb'))
