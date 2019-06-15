161c62b2e7988487977cbc0f65bc4645fe0634f6
alpha_caprice <romantcaregorodtcev@gmail.com>
2019-06-15 18:10:30 +0300

    done oop task

---
+++ 05-OOP_Exceptions/hw/oop_2.py
@@ -0,0 +1,169 @@
+"""
+В этом задании будем улучшать нашу систему классов из задания прошлой лекции
+(Student, Teacher, Homework)
+Советую обратить внимание на defaultdict из модуля collection для
+использования как общую переменную
+
+
+1. Как то не правильно, что после do_homework мы возвращаем все тот же
+объект - будем возвращать какой-то результат работы (HomeworkResult)
+
+HomeworkResult принимает объект автора задания, принимает исходное задание
+и его решение в виде строки
+Атрибуты:
+    homework - для объекта Homework, если передан не этот класс -  выкинуть
+    подходящие по смыслу исключение с сообщением:
+    'You gave a not Homework object'
+
+    solution - хранит решение ДЗ как строку
+    author - хранит объект Student
+    created - c точной датой и временем создания
+
+2. Если задание уже просрочено хотелось бы видеть исключение при do_homework,
+а не просто принт 'You are late'.
+Поднимайте исключение DeadlineError с сообщением 'You are late' вместо print.
+
+3. Student и Teacher имеют одинаковые по смыслу атрибуты
+(last_name, first_name) - избавиться от дублирования с помощью наследования
+
+4.
+Teacher
+Атрибут:
+    homework_done - структура с интерфейсом как в словаря, сюда поподают все
+    HomeworkResult после успешного прохождения check_homework
+    (нужно гаранитровать остутствие повторяющихся результатов по каждому
+    заданию), группировать по экземплярам Homework.
+    Общий для всех учителей. Вариант ипользования смотри в блоке if __main__...
+Методы:
+    check_homework - принимает экземпляр HomeworkResult и возвращает True если
+    ответ студента больше 5 символов, так же при успешной проверке добавить в
+    homework_done.
+    Если меньше 5 символов - никуда не добавлять и вернуть False.
+
+    reset_results - если передать экземпряр Homework - удаляет только
+    результаты этого задания из homework_done, если ничего не передавать,
+    то полностью обнулит homework_done.
+
+PEP8 соблюдать строго, проверку делаю автотестами и просмотром кода.
+Всем перечисленным выше атрибутам и методам классов сохранить названия.
+К названием остальных переменных, классов и тд. подходить ответственно -
+давать логичные подходящие имена.
+"""
+
+import datetime
+from collections import defaultdict
+
+
+class DeadlineError(Exception):
+    """ Error when deadline of task has passed"""
+
+
+class Person:
+
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __repr__(self):
+        return f"{self.last_name} {self.first_name}"
+
+
+class Homework:
+
+    def __init__(self, text: str, deadline: int):
+        self.text = text
+        self.deadline = datetime.timedelta(days=deadline)
+        self.created = datetime.datetime.now()
+
+    def __repr__(self):
+        return f"{self.text} {self.created.date()}"
+
+    def is_active(self):
+        return (datetime.datetime.now() - self.created) < self.deadline
+
+
+class Student(Person):
+
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+
+    def do_homework(self, homework: Homework, solution: str):
+        if not homework.is_active():
+            raise DeadlineError("You are late")
+        return HomeworkResult(self, homework, solution)
+
+
+class HomeworkResult:
+
+    def __init__(self, student: Student, homework: Homework, solution: str):
+        if not isinstance(homework, Homework):
+            raise AttributeError("You gave a not Homework object")
+        self.solution = solution
+        self.author = student
+        self.created = homework.created
+        self.homework = homework
+
+
+class Teacher(Person):
+    homework_done = defaultdict(set)
+
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+
+    @staticmethod
+    def create_homework(text: str, days: int):
+        return Homework(text, days)
+
+    def check_homework(self, homework: HomeworkResult):
+        solution_len_gt_5 = len(homework.solution) > 5
+        if solution_len_gt_5:
+            self.homework_done[homework.homework].add(homework.author)
+        return solution_len_gt_5
+
+    @classmethod
+    def reset_results(cls, homework=None):
+        if homework:
+            cls.homework_done.pop(homework)
+        else:
+            cls.homework_done.clear()
+
+
+if __name__ == '__main__':
+    opp_teacher = Teacher('Daniil', 'Shadrin')
+    advanced_python_teacher = Teacher('Aleksandr', 'Smetanin')
+
+    lazy_student = Student('Roman', 'Petrov')
+    good_student = Student('Lev', 'Sokolov')
+    print(opp_teacher)
+
+    oop_hw = opp_teacher.create_homework('Learn OOP', 1)
+    docs_hw = opp_teacher.create_homework('Read docs', 5)
+
+    result_1 = good_student.do_homework(oop_hw, 'I have done this hw')
+    result_2 = good_student.do_homework(docs_hw, 'I have done this hw too')
+    result_3 = lazy_student.do_homework(docs_hw, 'done')
+    result_4 = lazy_student.do_homework(oop_hw, "I have done this")
+    try:
+        result_4 = HomeworkResult(good_student, "fff", "Solution")
+    except Exception as e:
+        print(e)
+    opp_teacher.check_homework(result_1)
+    temp_1 = opp_teacher.homework_done
+
+    advanced_python_teacher.check_homework(result_1)
+    temp_2 = Teacher.homework_done
+    assert temp_1 == temp_2
+
+    opp_teacher.check_homework(result_2)
+    opp_teacher.check_homework(result_3)
+    opp_teacher.check_homework(result_4)
+
+    print(Teacher.homework_done[oop_hw])
+    print(Teacher.homework_done)
+    Teacher.reset_results()
+
+    bad_hw = opp_teacher.create_homework('Do nothing', -1)
+    try:
+        good_student.do_homework(bad_hw, "Done this")
+    except Exception as e:
+        print(e)
