2ecd66e1188e32e3bb9f9dbb46a1418e9e8dc666
alpha_caprice <romantcaregorodtcev@gmail.com>
2019-06-23 20:48:27 +0300

    first commit

---
+++ 07-network-basics/client.py
@@ -0,0 +1,77 @@
+import sys
+import socket
+import time
+import threading
+
+import window
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QMainWindow, QTextEdit,QAction, QFileDialog, QApplication, QToolBar
+
+class MyClient:
+    def __init__(self, server: tuple):
+        self.server = server
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.nickname = ""
+        self.online = False
+
+    def start(self):
+        self.client_socket.connect(self.server)
+        self.client_socket.sendto(self.nickname.encode("utf-8"), self.server)
+        self.online = True
+
+
+    def receiving(self, sock):
+        while self.online:
+            data, addr = sock.recvfrom(1024)
+            print(data.decode("utf-8"))
+            time.sleep(0.2)
+
+    def send_msg(self, msg):
+        try:
+            if msg:
+                self.client_socket.sendto(msg.encode(), self.server)
+                if msg == r"\exit":
+                    self.online = False
+            time.sleep(0.2)
+        except:
+            self.client_socket.sendto(r"\exit".encode("utf-8"), self.server)
+            self.online = False
+
+
+class App(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+        self.ui = window.Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.initUI()
+        self.client = MyClient(("192.168.0.103", 4242))
+
+    def initUI(self):
+        self.ui.pushButton.clicked.connect(self.send_msg)
+
+    def send_msg(self):
+        msg = self.ui.lineEdit.text()
+        if not self.client.online:
+            self.client.nickname = msg
+            self.client.start()
+            rT = threading.Thread(target=self.receive_msg)
+            rT.start()
+            self.ui.textEdit.append("You are join to chat!")
+        else:
+            self.ui.textEdit.append(f"You > {msg}")
+            self.client.send_msg(msg)
+        self.ui.lineEdit.clear()
+
+    def receive_msg(self):
+        while self.client.online:
+            data, addr = self.client.client_socket.recvfrom(1024)
+            self.ui.textEdit.append(data.decode())
+            time.sleep(0.2)
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    myapp = App()
+    myapp.show()
+    sys.exit(app.exec_())
---
+++ 07-network-basics/server.py
@@ -0,0 +1,60 @@
+import socket
+import time
+
+
+class ServerException(Exception):
+    """ This is server exception """
+
+
+class Server:
+    def __init__(self):
+        self.host = socket.gethostbyname(socket.gethostname())
+        self.port = 4242
+        self.clients = {}
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    def start(self):
+        self.server_socket.bind((self.host, self.port))
+        working = True
+        print("[ Server Started ]")
+
+        while working:
+            try:
+                data, addr = self.server_socket.recvfrom(1024)
+                data = data.decode()
+                if data == r"\exit":
+                    self.clients.pop(addr)
+                    msg_left = f"{self.clients[addr]} left the chat!"
+                    self.print_server_msg(msg_left, addr)
+                    self.broadcast(msg_left, addr)
+                    continue
+
+                if addr not in self.clients:
+                    self.clients[addr] = data
+                    msg = f"{self.clients[addr]} is join the chat"
+                    self.print_server_msg(msg, addr)
+                    self.broadcast(msg, addr)
+                else:
+                    self.broadcast(data, addr)
+                    self.print_server_msg(data, addr)
+
+            except ServerException:
+                print("\n[ Server Stopped ]")
+                working = False
+
+        self.server_socket.close()
+
+    def print_server_msg(self, msg: str, addr: tuple):
+        time_ = time.strftime("%Y-%m-%d-%H.%M.%S", time.localtime())
+        print(f"[{addr[0]}]=[{addr[1]}]=[{time_}]/", end="")
+        print(f"{self.clients[addr]}: {msg}")
+
+    def broadcast(self, msg: str, not_send: tuple):
+        msg = f"{self.clients[not_send]} > {msg}".encode()
+        for client in self.clients:
+            if not_send != client:
+                self.server_socket.sendto(msg, client)
+
+if __name__ == "__main__":
+    my_server = Server()
+    my_server.start()
